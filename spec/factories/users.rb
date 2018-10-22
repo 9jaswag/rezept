1e4cd0ee7bd7962e87ff71cebbba14a165ed0c5e
Chuks Opia <chuks24ng@yahoo.co.uk>
2018-10-22 21:18:53 +0100

    chore: install active storage

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,3 +1,26 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id                :bigint(8)        not null, primary key
+#  activated         :boolean
+#  activated_time    :datetime
+#  activation_digest :string
+#  email             :string
+#  is_admin          :boolean          default(FALSE)
+#  password_digest   :string
+#  reset_digest      :string
+#  reset_time        :datetime
+#  username          :string
+#  created_at        :datetime         not null
+#  updated_at        :datetime         not null
+#
+# Indexes
+#
+#  index_users_on_email     (email)
+#  index_users_on_username  (username)
+#
+
 FactoryBot.define do
   factory :user do
     username { Faker::Name.unique.first_name[1..15] }
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,3 +1,26 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id                :bigint(8)        not null, primary key
+#  activated         :boolean
+#  activated_time    :datetime
+#  activation_digest :string
+#  email             :string
+#  is_admin          :boolean          default(FALSE)
+#  password_digest   :string
+#  reset_digest      :string
+#  reset_time        :datetime
+#  username          :string
+#  created_at        :datetime         not null
+#  updated_at        :datetime         not null
+#
+# Indexes
+#
+#  index_users_on_email     (email)
+#  index_users_on_username  (username)
+#
+
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
